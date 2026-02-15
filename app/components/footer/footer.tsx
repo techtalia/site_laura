5523cf7d64b3ea8ca517a28fa7cc653fc0f99a93
Talia Andrade <taliaandrade.contato@gmail.com>
2026-02-15 14:10:03 -0300

    responsive header and footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,32 +1,32 @@
 import Image from "next/image";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="w-full flex justify-center gap-x-20 bg-(--laranja-apagado) h-[15vh] items-center  ">
-      {/* Logo */}
-      <div>Logo</div>
+    <div className="w-full flex justify-center gap-x-20 bg-(--laranja-apagado) min-h-[15vh] items-center footer ">
+      
 
       {/* Telefone */}
       <div className="flex flex-col items-center justify-center space-y-4">
-        <p>Telefone</p>
+        <p className="font-bold">Telefone</p>
         <p>(16) 99783-3995</p>
       </div>
 
       {/* Instagram */}
-      <div className="flex flex-col items-center justify-center space-y-4">
-        <p>Instagram</p>
+      <div className="flex flex-col items-center justify-center space-y-4 flex-wrap">
+        <p className="font-bold">Instagram</p>
         <p>@psi.lauraleal</p>
       </div>
       
       {/* Email */}
       <div className="flex flex-col items-center justify-center space-y-4">
-        <p>Email</p>
+        <p className="font-bold">Email</p>
         <p>lauraleal.psi@gmail.com</p>
       </div>
 
       {/* Redes Sociais */}
       <div className="flex flex-col items-center justify-center space-y-4">
-        <p>Redes Sociais</p>
+        <p className="font-bold">Redes Sociais</p>
         <div className="flex gap-4">
           <a href="https://wa.me/5516997833995" target="_blank">
             <Image
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,16 +1,37 @@
+'use client'
 import './navbar.css'
 import Link from 'next/link'
 
 const Navbar = () => {
+  const toggleMenu = () => {
+    const menuLateral = document.querySelector('.menuLateral');
+    const menuBurguer = document.querySelector('.menuBurguer');
+    if (menuLateral) {
+      menuLateral.classList.toggle('active');
+    }
+    if (menuBurguer) {
+      menuBurguer.classList.toggle('active');
+    }
+    
+  }
   return (
-        <ul className='h-[10vh] px-16 bg-(--marrom-apagado) text-white flex items-center justify-end gap-8 '>
-            <li className='mr-auto'><Link href="/">Início</Link></li>
-            <li><Link href="/sobre">Sobre mim</Link></li>
-            <li><Link href="/abordagem">Abordagem</Link></li>
-            <li><Link href="/areasDeAtendimento">Áreas de Atendimento</Link></li>
-            <li><Link href="/servicos">Meus Serviços</Link></li>
+    <nav className='flex h-[10vh] px-16 bg-(--marrom-apagado) items-center w-screen justify-between text-white'>
+          <Link href="/">Início</Link>
+          <div className='menuBurguer' onClick={toggleMenu}>
+            <div className='bar1'></div>
+            <div className='bar2'></div>
+            <div className='bar3'></div>
+          </div>
+
+        <ul className='flex gap-8 menuLateral '>
+            <li><Link href="/sobre" onClick={toggleMenu}>Sobre mim</Link></li>
+            <li><Link href="/abordagem" onClick={toggleMenu}>Abordagem</Link></li>
+            <li><Link href="/areasDeAtendimento" onClick={toggleMenu}>Áreas de Atendimento</Link></li>
+            <li><Link href="/servicos" onClick={toggleMenu}>Meus Serviços</Link></li>
 
         </ul>
+
+    </nav>
   )
 }
 
